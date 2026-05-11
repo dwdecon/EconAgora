6aa98c502d9671866061b40ec62d5206cde29bc5
david <18010759597@163.com>
2026-05-11 23:10:21 +0800

    chore: add @next/bundle-analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+
 const nextConfig = {
   output: "standalone",
 };
 
-export default withNextIntl(nextConfig);
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default bundleAnalyzer(withNextIntl(nextConfig));
